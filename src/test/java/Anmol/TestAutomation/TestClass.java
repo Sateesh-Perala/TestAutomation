41ef33077e822251160c7fc8172430da1da99b14
Sateesh-Perala <72450723+Sateesh-Perala@users.noreply.github.com>
2020-12-03 20:30:15 +0530

    Very new commit

--- src/test/java/Anmol/TestAutomation/TestClass.java
+++ src/test/java/Anmol/TestAutomation/TestClass.java
@@ -10,32 +10,31 @@ import org.testng.annotations.Test;
 
 public class TestClass {
 
-   public static WebDriver driver;
+	public static WebDriver driver;
+
 	@BeforeMethod
 	public void lanuchDriver() {
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome-stable");
-// <<<<<<< HEAD
-		//System.setProperty("webdriver.gecko.driver", "D:\\GeckoDriver\\geckodriver-v0.28.0-win64\\geckodriver.exe");
-		//WebDriver driver = new FirefoxDriver();
-		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+		// System.setProperty("webdriver.gecko.driver",
+		// "D:\\GeckoDriver\\geckodriver-v0.28.0-win64\\geckodriver.exe");
+		// WebDriver driver = new FirefoxDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-// =======
-	driver = new ChromeDriver();
-		driver.manage().window().fullscreen();
-// >>>>>>> df7b613a31479daef8eaab20c93d31c907bd4be1
 	}
 
 	@Test
 	public void Test1() {
-		driver.manage().window().maximize();
+		
 		driver.navigate().to("http://aisqa.dhanushinfotech.com/");
+		driver.manage().window().maximize();
 		System.out.println("Test1 Title is : " + driver.getTitle());
 	}
 
 	@Test
 	public void Test2() {
-		driver.manage().window().maximize();
+		
 		driver.get("http://aisqa.dhanushinfotech.com/");
+		driver.manage().window().maximize();
 		driver.findElement(By.id("username")).sendKeys("admin@anmol.com");
 		driver.findElement(By.id("password")).sendKeys("98765");
 		driver.findElement(By.tagName("button")).click();
@@ -55,9 +54,6 @@ public class TestClass {
 }
 
 /*
-	public static void main(String [] args) {
-	TestClass t= new TestClass();
-			t.lanuchDriver();
-			t.Test1();
-			t.Test2();
-} */
+ * public static void main(String [] args) { TestClass t= new TestClass();
+ * t.lanuchDriver(); t.Test1(); t.Test2(); }
+ */
